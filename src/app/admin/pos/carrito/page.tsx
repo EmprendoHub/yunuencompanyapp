8fa452fa80bdd728a16300fd457f36abe6253df9
Emprendo Mac <emprendomex@gmail.com>
2024-10-02 10:43:57 -0600

    update pos Cart admin

--- src/app/admin/pos/carrito/page.tsx
+++ src/app/admin/pos/carrito/page.tsx
@@ -1,10 +1,14 @@
-import POSCart from '@/components/pos/POSCart';
-import React from 'react';
+import POSCart from "@/components/pos/POSCart";
+import React from "react";
+import { getServerSession } from "next-auth";
+import { options } from "../../../api/auth/[...nextauth]/options";
 
 const POSCartPage = async () => {
+  const session = await getServerSession(options);
+  const userId = session.user._id;
   return (
     <>
-      <POSCart />
+      <POSCart userId={userId} />
     </>
   );
 };
