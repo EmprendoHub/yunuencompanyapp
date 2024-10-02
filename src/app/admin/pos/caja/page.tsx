53a6e2a7855b07daced73f258830164d537718ca
Emprendo Mac <emprendomex@gmail.com>
2024-10-02 11:26:12 -0600

    update user ID

--- src/app/admin/pos/caja/page.tsx
+++ src/app/admin/pos/caja/page.tsx
@@ -1,7 +1,11 @@
 import POSPaymentForm from "@/components/pos/POSPaymentForm";
+import { getServerSession } from "next-auth";
+import { options } from "../../../api/auth/[...nextauth]/options";
 
 const DrawerPage = async () => {
-  return <POSPaymentForm />;
+  const session = await getServerSession(options);
+  const userId = session.user._id;
+  return <POSPaymentForm userId={userId} />;
 };
 
 export default DrawerPage;
